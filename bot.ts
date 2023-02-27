7d3ca82d07f01c1bb276fc115e3e0b7ea06f84b3
Cyrille Derche <c.derche@me.com>
2023-02-27 14:46:11 +0100

    add markdown

--- bot.ts
+++ bot.ts
@@ -1,11 +1,12 @@
 import { Bot } from "grammy";
 import dotenv from "dotenv";
 
+// import token from .env file
 dotenv.config()
 const token = String(process.env.TOKEN)
 
 const bot = new Bot(token)
 
-bot.command("chat_id", ctx => ctx.reply(ctx.chat.id.toString()))
+bot.command("chat_id", ctx => ctx.reply("`" + ctx.chat.id.toString() + "`", { parse_mode: "MarkdownV2"}))
 
 bot.start();
